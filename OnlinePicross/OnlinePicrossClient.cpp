81c1067232cdc1ab773e4f76d6a25e9cdaf2ea06
elflee77 <elflee77@gmail.com>
2022-09-22 15:28:54 +0900

    Lecture1

---
+++ OnlinePicross/OnlinePicrossClient.cpp
@@ -0,0 +1,63 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+static void error_callback(int error, const char* description)
+{
+    fputs(description, stderr);
+}
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main()
+{
+    GLFWwindow* window;
+    
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, key_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float ratio;
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float)height;
+
+
+        glClearColor(0, 0, 1, 1);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(0.0f, 0.5f);
+        glColor3f(0.4f, 0.72f, 0.1f);
+
+        glVertex2f(-0.5f, -0.5f);
+        glColor3f(0.4f, 0.0f, 0.0f);
+
+        glVertex2f(0.5f, -0.5f);
+        glColor3f(0.0f, 0.0f, 0.0f);
+
+        glEnd();
+        glFlush();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # OnlinePicross
+
+* VCPKG 사용함
+    * https://vcpkg.io/en/getting-started.html
