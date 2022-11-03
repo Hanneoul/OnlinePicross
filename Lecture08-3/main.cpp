9e2923b45a27af6eb02ead96cfd018486468d2e1
elflee77 <elflee77@gmail.com>
2022-11-03 15:47:32 +0900

    .

--- Lecture08-3/main.cpp
+++ Lecture08-3/main.cpp
@@ -1,6 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 
 #define WIDTH 7
 #define HEIGHT 6
@@ -20,6 +23,157 @@ void CursorView()
 	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 }
 
+
+void DrawCursor(int coordX, int coordY)
+{
+}
+
+void DrawGamePlayMap(int width, int height, int coordX, int coordY, int* image, int cursorX, int cursorY)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < width + 2; i++)
+		for (j = 0; j < height + 2; j++)
+		{
+			gotoxy(coordX + i * 2, coordY + j);
+
+			if (j == 0 || j == height + 1)
+				printf("--");
+			else if (i == 0)
+				printf("|");
+			else if (i == width + 1)
+				printf(" |");
+			else
+			{
+				if (image[WIDTH * (j - 1) + (i - 1)] == 0)
+				{
+					if (i - 1 == cursorX && j - 1 == cursorY)
+					{
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
+						printf("бр");
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+					}
+					else
+					{
+						printf("бр");
+					}
+
+
+				}
+				else
+				{
+					if (i - 1 == cursorX && j - 1 == cursorY)
+					{
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
+						printf("бс");
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+					}
+					else
+					{
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
+						printf("бс");
+						SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+					}
+				}
+			}
+		}
+}
+
+void DrawClearMap(int width, int height, int coordX, int coordY, int* image)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+	
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; i < width + 2; i++)
+		for (j = 0; j < height + 2; j++)
+		{
+			gotoxy(coordX + i * 2, coordY + j);
+
+			if (j == 0 || j == height + 1)
+				printf("--");
+			else if (i == 0)
+				printf("|");
+			else if (i == width + 1)
+				printf(" |");
+			else
+			{
+				if (image[WIDTH * (j - 1) + (i - 1)] == 0)
+				{
+					printf("бр");
+				}
+				else
+				{
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
+					printf("бс");					
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
+				}
+			}
+		}
+}
+
+struct vec2
+{
+	int x;
+	int y;
+};
+
+vec2 DrawNumbers(int width, int height, int* image, char** wText, char** hText)
+{
+	vec2 MapStartPos;
+	MapStartPos.x = 0;
+	MapStartPos.y = 0;
+
+	int continuityCount = 0;
+	int indexX = 0;
+	int indexY = 0;
+	int MaxlineXStartPos = 0;
+	
+	continuityCount = 0;
+
+	
+	if (width == 1)
+	{
+	}		
+	
+	for (indexY = 0; indexY < height; indexY++)
+	{
+		continuityCount = 0;
+		wText[indexY] = (char*)"";
+
+		for (indexX = 0; indexX < width - 1; indexX++)
+		{
+			if (image[(indexY * width) + indexX] == 1 && image[(indexY * width) + indexX + 1] == 1)
+				continuityCount++;
+			else
+			{
+				char buffer[35];
+				char* a = _itoa(continuityCount, buffer, 10);
+				wText[indexY] = strcat(a, wText[indexY]);
+								
+				continuityCount = 0;
+				MaxlineXStartPos += 2;
+			}
+		}
+	}
+
+	for (indexY = 0; indexY < height; indexY++)
+	{
+		gotoxy(0, indexY);
+		printf("%s\n", wText[indexY]);
+	}
+
+
+
+	return MapStartPos;
+}
+
+
+
 int main(int argc, char* argv[])
 {
 	CursorView();
@@ -35,15 +189,15 @@ int main(int argc, char* argv[])
 		   0,0,0,1,0,0,0,
 	};
 
-	LPCSTR wText[HEIGHT];
-	LPCSTR hText[WIDTH];
+	char* wText[HEIGHT];
+	char* hText[WIDTH];
 
 	int cw = 0;
 	int ch = 0;
 	int maxWSpace = 0;
 	int maxHSpace = 0;
 
-	for(ch=0;ch<HEIGHT; ch++)
+	/*for(ch=0;ch<HEIGHT; ch++)
 	{
 		int wCount = 0;
 		for (cw = 0; cw < WIDTH-1; cw++)
@@ -67,7 +221,7 @@ int main(int argc, char* argv[])
 				wText[ch] += ' ';
 			}
 		}
-	}
+	}*/
 
 
 	while (1)
@@ -112,54 +266,11 @@ int main(int argc, char* argv[])
 		}
 			
 
-		int i = 0;
-		int j = 0;
-		for (i = 0; i < WIDTH + 2; i++)
-			for (j = 0; j < HEIGHT + 2; j++)
-			{
-				gotoxy(i * 2, j);
-
-				if (j == 0 || j == HEIGHT + 1)
-					printf("--");
-				else if (i == 0)
-					printf("|");
-				else if (i == WIDTH + 1)
-					printf(" |");
-				else
-				{
-					if (Image[WIDTH * (j - 1) + (i - 1)] == 0)
-					{
-						if (i - 1 == cursor[0] && j - 1 == cursor[1])
-						{
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
-							printf("бр");
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
-						}
-						else
-						{
-							printf("бр");
-						}
-						
-						
-					}
-					else
-					{
-						if (i - 1 == cursor[0] && j - 1 == cursor[1])
-						{
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 13);
-							printf("бс");
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
-						}
-						else
-						{
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 2);
-							printf("бс");
-							SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
-						}
-					}
-				}
-			}
-
+		//DrawClearMap(WIDTH, HEIGHT, 0, 0, Image);
+		//DrawGamePlayMap(WIDTH, HEIGHT, 0, 0, Image, cursor[0], cursor[1]);
+		
+		DrawNumbers(WIDTH, HEIGHT, Image, wText, hText);
+		
 	}
 
 	return 0;
